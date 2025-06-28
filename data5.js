5cc789df96d755599fd1b33e665e4bd2e5d7ee31
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-06-28 11:46:58 +0545

    updatedtrainees

--- data5.js
+++ data5.js
@@ -1,31 +1,31 @@
 // data5.js - Employees Dataset (Additional Data)
-var data5 = `Name,	Phone No.
-"1. श्री विश्वदीप बेस्रा (Shri Vishwadeep Besra),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,biswadipbesra@gmail.com",	9849655612
-"2. श्री हरिकृष्ण के. सी. (Shri Harikrishna K.C.),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,Hari7kc@gmail.com",	9841411908
-"3. श्री प्रकाश न्यौपाने (Shri Prakash Neupane),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,n/a",	9849155012
-"4. श्री आस्था सम्मान (Shri Aastha Samman),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,Lamsalaasthal@gmail.com",	9843015959
-"5. श्री भूमिनन्द खनाल (Shri Bhuminand Khanal),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,khanalbhumi@gmail.com",	9841304983
-"6. श्री लोकहरी कडेल (Shri Lokhari Kandel),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,kandellhari@gmail.com",	9841299063
-"7. श्री ईश्वरीप्रसाद आचार्य (Shri Ishwariprasad Acharya),उपसचिव,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,acharyaishwariprasad@gmail.com",	9847107362
-"8. श्री दीपा बली (Deepa Bali),उपसचिव,वन तथा वातावरण मन्त्रालय,dpaolichhetri@gmail.com",	9851271123
-"9. श्री प्रभात क्षेत्री (Prabhat Kshetri),उपन्यायाधिवक्ता,महान्यायाधिवक्ताको कार्यालय,prabhatchhetri7@gmail.com",	9860453191
-"10. श्री रमेश प्रसाद पौडेल (Ramesh Prasad Poudel),सि.डि.ई.,भौतिक पूर्वाधार तथा यातायात मन्त्रालय,rameshppoudel@gmail.com",	9841822260
-"11. श्री शिवप्रसाद आचार्य (Shri Shivprasad Acharya),इजलास अधिकृत,सर्वोच्च अदालत,spacharya43@gmail.com",	9849667879
-"12. श्री छविलाल पौडेल (Shri Chhabilal Poudel),उपसचिव,गृह मन्त्रालय,moha.chhabipdl@gmail.com",	9851296633
-"13. श्री मिलन भट्टराई (Shri Milan Bhattarai),उपसचिव,महिला,बालबालिका तथा ज्येष्ठ नागरिक मन्त्रालय,m.bhattarai012@gmail.com",	9851009287
-"14. श्री अद्वैत बराल (Shri Adwait Baral),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,adwait.baral1995@gmail.com",	9443413500
-"15. श्री निरा गिरी (Shri Nira Giri),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,neeragiri2016@gmail.com",	9808091767
-"16. श्री भावना वर्णवाल (Shri Bhavana Barnawal),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,bhawanabarnawal@gmail.com",	n/a
-"17. श्री दिपशिखा वाग्ले (Shri Deepshikha Wagle),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,deepshikhawagle@gmail.com",	9849997474
-"18. श्री निर्मला थापा (Shri Nirmala Thapa),n/a,सञ्चार तथा सूचना प्रविधि मन्त्रालय,nirvikthapa4@gmail.com",	9849164623
-"19. श्री खिमानन्द ज्ञवाली (Shri Khimanand Gyanwali),n/a,ऊर्जा,जलस्रोत तथा सिंचाइ मन्त्रालय,Khemgyawali123@gmail.com",	9860776051
-"20. श्री सुप्रिया सिलवाल (Shri Supriya Silwal),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,silwalsupriya@gmail.com",	9849063851
-"21. श्री अनुजा दत्त (Shri Anuja Datt),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,me.anujasut@gmail.com",	9844029757
-"22. श्री कुसुम पन्त (Shri Kusum Pant),शाखा अधिकृत,परराष्ट्र मन्त्रालय,kusumpanta82@gmail.com",	9808897948
-"23. श्री होम प्रसाद पराजुली (Shri Hom Prasad Parajuli),n/a,अर्थ मन्त्रालय,homparajuli2@gmail.com",	9844694491
-"24. श्री लक्ष्मी शर्मा (Shri Laxmi Sharma),n/a,शहरी विकास मन्त्रालय,Lslaxmi09@gmail.com",	9841712880
-"25. श्री हुमावती ढकाल (Shri Humawati Dhakal),शाखा अधिकृत,कानून,न्याय तथा संसदीय मामिला मन्त्रालय,humawatidhakal@gmail.com",	9849870373
-"26. श्री एशु बस्नेत (Shri Ashu Basnet),शाखा अधिकृत,परराष्ट्र मन्त्रालय,basnetaashu@gmail.com",	9841905473`;
+var data5 = `Name, 	Phone No.
+"1. श्री विश्वदीप बेस्रा (Shri Vishwadeep Besra), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, biswadipbesra@gmail.com", 	9849655612
+"2. श्री हरिकृष्ण के. सी. (Shri Harikrishna K.C.), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, Hari7kc@gmail.com", 	9841411908
+"3. श्री प्रकाश न्यौपाने (Shri Prakash Neupane), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, prakashachhami@gmail.com", 	9849155012
+"4. श्री आस्था सम्मान (Shri Aastha Samman), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, Lamsalaasthal@gmail.com", 	9843015959
+"5. श्री भूमिनन्द खनाल (Shri Bhuminand Khanal), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, khanalbhumi@gmail.com", 	9841304983
+"6. श्री लोकहरी कडेल (Shri Lokhari Kandel), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, kandellhari@gmail.com", 	9841299063
+"7. श्री ईश्वरीप्रसाद आचार्य (Shri Ishwariprasad Acharya), उपसचिव, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, acharyaishwariprasad@gmail.com", 	9847107362
+"8. श्री दीपा बली (Deepa Bali), उपसचिव, वन तथा वातावरण मन्त्रालय, dpaolichhetri@gmail.com", 	9851271123
+"9. श्री प्रभात क्षेत्री (Prabhat Kshetri), उपन्यायाधिवक्ता, महान्यायाधिवक्ताको कार्यालय, prabhatchhetri7@gmail.com", 	9860453191
+"10. श्री रमेश प्रसाद पौडेल (Ramesh Prasad Poudel), सि.डि.ई., भौतिक पूर्वाधार तथा यातायात मन्त्रालय, rameshppoudel@gmail.com", 	9841822260
+"11. श्री शिवप्रसाद आचार्य (Shri Shivprasad Acharya), इजलास अधिकृत, सर्वोच्च अदालत, spacharya43@gmail.com", 	9849667879
+"12. श्री छविलाल पौडेल (Shri Chhabilal Poudel), उपसचिव, गृह मन्त्रालय, moha.chhabipdl@gmail.com", 	9851296633
+"13. श्री मिलन भट्टराई (Shri Milan Bhattarai), उपसचिव, महिला, बालबालिका तथा ज्येष्ठ नागरिक मन्त्रालय, m.bhattarai012@gmail.com", 	9851009287
+"14. श्री अद्वैत बराल (Shri Adwait Baral), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, adwait.baral1995@gmail.com", 	9443413500
+"15. श्री निरा गिरी (Shri Nira Giri), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, neeragiri2016@gmail.com", 	9808091767
+"16. श्री भावना वर्णवाल (Shri Bhavana Barnawal), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, bhawanabarnawal@gmail.com", 	9845310077
+"17. श्री दिपशिखा वाग्ले (Shri Deepshikha Wagle), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, deepshikhawagle@gmail.com", 	9849997474
+"18. श्री निर्मला थापा (Shri Nirmala Thapa), n/a, सञ्चार तथा सूचना प्रविधि मन्त्रालय, nirvikthapa4@gmail.com", 	9849164623
+"19. श्री खिमानन्द ज्ञवाली (Shri Khimanand Gyanwali), उपसचिव, ऊर्जा, जलस्रोत तथा सिंचाइ मन्त्रालय, Khemgyawali123@gmail.com", 	9860776051
+"20. श्री सुप्रिया सिलवाल (Shri Supriya Silwal), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, silwalsupriya@gmail.com", 	9849063851
+"21. श्री अनुजा दत्त (Shri Anuja Datt), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, me.anujasut@gmail.com", 	9844029757
+"22. श्री कुसुम पन्त (Shri Kusum Pant), शाखा अधिकृत, परराष्ट्र मन्त्रालय, kusumpanta82@gmail.com", 	9808897948
+"23. श्री होम प्रसाद पराजुली (Shri Hom Prasad Parajuli), शाखा अधिकृत, अर्थ मन्त्रालय, homparajuli2@gmail.com", 	9844694491
+"24. श्री लक्ष्मी शर्मा (Shri Laxmi Sharma), उपसचिव, शहरी विकास मन्त्रालय, Lslaxmi09@gmail.com", 	9841712880
+"25. श्री हुमावती ढकाल (Shri Humawati Dhakal), शाखा अधिकृत, कानून, न्याय तथा संसदीय मामिला मन्त्रालय, humawatidhakal@gmail.com", 	9849870373
+"26. श्री एशु बस्नेत (Shri Ashu Basnet), शाखा अधिकृत, परराष्ट्र मन्त्रालय, basnetaashu@gmail.com", 	9841905473`;
 
 // Dataset metadata
 var data5Info = {
