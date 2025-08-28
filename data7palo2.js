b2c9fa1ba071ef4f08c4314832fd6b02b206e436
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-28 14:34:46 +0545

    Update data7palo2.js

--- data7palo2.js
+++ data7palo2.js
@@ -4,8 +4,8 @@ var data7palo2 = `sno1,sno2, date, day of week,benchofficer1,benchofficer2,faish
 03,3,20820421,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
 04,4,20820428,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
 05,5,20820504,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
-05,6,20820511,wednesday,shiva,rajendra,1,0,bench held,गुठी सम्बन्धी मुद्दा नि.सु रहेको,सपना श्रीमान अकस्मात विरामी भएकोले निसु रहन गएको,निसुको मिति नतोकिएको
-05,6,20820518,wednesday,basanta,shiva,,,,,,
+06,6,20820511,wednesday,shiva,rajendra,1,0,bench held,गुठी सम्बन्धी मुद्दा नि.सु रहेको,सपना श्रीमान अकस्मात विरामी भएकोले निसु रहन गएको,निसुको मिति नतोकिएको
+07,7,20820518,wednesday,basanta,shiva,,,,,,
 
 `;
 
