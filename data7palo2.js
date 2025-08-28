18a69f39a17fea34ae5d45da563be251a2f62640
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-28 14:26:09 +0545

    Update data7palo2.js

--- data7palo2.js
+++ data7palo2.js
@@ -1,10 +1,11 @@
-var data7palo2 = `sno1,sno2, date, day of week,benchofficer1,benchofficer2,faishala, orders,benchstatus,remark1,remark2
-01,1,20820407,wednesday,,,0,0,no causelist,सालतामेलीको कारण कुनै पेशी नराखिएको,,
-02,2,20820414,wednesday,,,0,0,no causelist,सालतामेलीको कारण कुनै पेशी नराखिएको,,
-03,3,20820421,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,
-04,4,20820428,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,
-05,5,20820504,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,
-05,6,20820511,wednesday,shiva,rajendra,,,,,,
+var data7palo2 = `sno1,sno2, date, day of week,benchofficer1,benchofficer2,faishala, orders,benchstatus,remark1,remark2,remark3
+01,1,20820407,wednesday,,,0,0,no causelist,सालतामेलीको कारण कुनै पेशी नराखिएको,,,
+02,2,20820414,wednesday,,,0,0,no causelist,सालतामेलीको कारण कुनै पेशी नराखिएको,,,
+03,3,20820421,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
+04,4,20820428,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
+05,5,20820504,wednesday,,,0,0,bench not held,गोलाप्रथाको विवादले पेशीसूचि नै प्रकाशित भएन,,,
+05,6,20820511,wednesday,shiva,rajendra,1,0,bench held,गुठी सम्बन्धी मुद्दा नि.सु रहेको,सपना श्रीमान अकस्मात विरामी भएकोले निसु रहेको,निसुको मिति नतोकिएको
+05,6,20820518,wednesday,basanta,shiva,,,,,,
 
 `;
 
