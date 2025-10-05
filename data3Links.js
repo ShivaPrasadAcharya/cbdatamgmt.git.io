c09a7a4b7937527238042c1cd23f6ba890a6bef8
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-10-05 13:04:56 +0000

    ok

--- data3Links.js
+++ data3Links.js
@@ -53,6 +53,7 @@ var data3Links = `ID,Subject,Remark,Link
 0027,court order, list of non executed mandamus,https://ekantipur.com/news/2025/09/08/144-decrees-of-the-supreme-court-are-in-abeyance-social-media-is-being-interpreted-and-implemented-overnight-27-59.html
 0028,fundamental right, judicial administration-denial to registration case,https://www.onlinekhabar.com/2025/09/1759775/arbitrary-order-of-the-judicial-administration-against-the-fundamental-rights-of-citizens
 0029,GenZ revolution,failed state,https://www.nayapatrikadaily.com/news-details/177480/2025-09-10
+0030,Astrology vs Science-disaster,harihar adhikari's immature prediction,https://www.ratopati.com/story/516501/human-casualties-caused-by-wrong-information-about-astrology-while-trekking-in-langtang-kulman-ghising
 
 
 `;
