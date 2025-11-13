0d15e02a120a9192724284871aeaf49470863d09
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-11-13 15:37:38 +0000

    ok

--- data5Url2.js
+++ data5Url2.js
@@ -1,9 +1,9 @@
 var data5Url2 = `sno,subject,description,shorturl,longurl
 01,NJA article-India,see other series too,Judicial Accountability,https://nja.gov.in/Jounals_Publications_Newsletters/NJA%20Ocacasional%20Paper%20Series%20No.1.pdf
-02,Order,nisu with date,open nisu,Orders/CamScanner 02-02-2025 11.19.pdf
-03,Order,,,Orders/CamScanner 04-06-2025 15.57.pdf
-04,Order,,,Orders/CamScanner 05-06-2025 09.50.pdf
-05,Order,,,Orders/CamScanner 05-06-2025 11.27.pdf
+02,Order,मिति तोकेर बहसनोट र निसु,open nisu,Orders/CamScanner 05-06-2025 11.27.pdf
+03,Order,लगतकट्टाको महत्व,open lagat katta,Orders/CamScanner 04-06-2025 15.57.pdf
+04,Order,विश्वम्भर श्रीमानबाट लिखित रूपमा धन्यवाद व्यक्त,open thank you,Orders/CamScanner 05-06-2025 09.50.pdf
+05,Order,,,Orders/CamScanner 02-02-2025 11.19.pdf
 06,Order,,,Orders/CamScanner 06-02-2024 12.57.pdf
 07,Order,,,Orders/CamScanner 06-03-2024 16.42.pdf
 08,Order,,,Orders/CamScanner 14-06-2025 13.44.pdf
